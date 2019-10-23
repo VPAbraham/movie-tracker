d28e90f442c7e5dc57e4cbfa10006af403da6d51
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-22 20:19:24 -0600

    Fetch data for movies now playing upon componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieData: []
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=28964418fdafb10fc97bbbad131d01c3&language=en-US&page=1')
+    .then(r => r.json())
+    .then(data => this.setState({ movieData: data }))
+  }
+
   render() {
     return (
       <div className="App">
