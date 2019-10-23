0861f8ff8cb096fbd2e3a980e5e8515f71772ccb
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-22 20:31:39 -0600

    Create separate directory for App within Containers and move App files into it

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -18,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p> -- APP -- </p>
+        <Route exact path='/' component={} />
       </div>
     )
   }
