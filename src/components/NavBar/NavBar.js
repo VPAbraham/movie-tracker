6e340222c8d8bcc93f316c4cc63d5fc5a6a8cbd0
VPAbraham <vpa456@gmail.com>
2019-10-24 14:21:58 -0600

    Initialize an assets folder with an images file to store al of our imported images into an images object. Create NavBar component. Some minor styling changes.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.scss';
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="nav-bar">
+      <h1>Movie Tracker</h1>
+    </nav>
+  )
+}
+
+export default NavBar;
