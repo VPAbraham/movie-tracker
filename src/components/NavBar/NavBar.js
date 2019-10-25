e463d9f9df48bb6355089a172b4b855412e38f5c
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-10-24 20:17:56 -0600

    Do styling for the container

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import React from 'react';
 const NavBar = () => {
   return (
     <nav className="nav-bar">
-      <h1 className="main-logo">Movie<span className='logo-second'>Trac</span></h1>
+      <h1 className="main-logo"><div className="hidden-div"><span className="hidden-letters">M</span><span className="hidden-letters">T</span></div></h1>
       <div className="menu">
         <span className="menu-item">Favorite Movies</span>
         <span className="menu-item">Prob another thing</span>      
