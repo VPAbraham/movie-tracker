b4a00a12a681898e8d97291f85ebac89ff0accc3
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-10-28 20:49:11 -0600

    Fix broke tests

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 import { setCurrentUser, logOut } from '../../actions';
 
 
-const NavBar = ({ isLoggedIn, setCurrentUser, logOut }) => {
+export const NavBar = ({ isLoggedIn, setCurrentUser, logOut }) => {
   const signOut = () => {
     setCurrentUser("", "");
     logOut();
