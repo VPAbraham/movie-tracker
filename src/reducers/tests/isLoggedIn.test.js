b9fd64a364b698373c8a386a92101c36e0894d2f
Jessie Le-Ho <lehoj@oregonstate.edu>
2019-10-29 14:46:38 -0600

    Pass tests

--- src/reducers/tests/isLoggedIn.test.js
+++ src/reducers/tests/isLoggedIn.test.js
@@ -1,47 +1,43 @@
 import { isLoggedIn } from '../isLoggedIn'
 
 describe('isLoggedIn', () => {
-    it.skip('should shut the hell up as well', () => {
+    it('should return the initial state', () => {
+        const expected = false;
 
+        const result = isLoggedIn(undefined, false)
+
+        expect(result).toEqual(expected)
     })
     it.skip('should be able to log in a user', () => {
         const action = {
-            type: 'TOGGLE_LOGIN',
+            type: 'LOGIN',
             loggedIn: true
         }
-        const state = [
-            { name: 'Eric', email: 'eric@gmail.com' , password:'password', id: 5 , loggedIn: true },
-            { name: 'Jessie', email: 'jessie@yahoo.com', password: 'password', id: 1, loggedIn: true },
-            { name: 'Victor' , email: 'victor@hotmail.com', password: 'password', id: 6, loggedIn: true }
-        ]
-        const newState = [
-            { name: 'Eric', email: 'eric@gmail.com', password: 'password', id: 5, loggedIn: true },
-            { name: 'Jessie', email: 'jessie@yahoo.com', password: 'password', id: 1, loggedIn: true },
-            { name: 'Victor', email: 'victor@hotmail.com', password: 'password', id: 6, loggedIn: false }
-        ]
+        const state = 
+            { name: 'Eric', email: 'eric@gmail.com' , password:'password', id: 5 , loggedIn: false }
+        
+        const newState = 
+            { name: 'Eric', email: 'eric@gmail.com', password: 'password', id: 5, loggedIn: true }
+        
 
-        const result = users(state, action);
+        const result = isLoggedIn(state, action);
 
         expect(result).toEqual(newState);
     });
-    it.skip('should be able to toggle a user\'s login status to false', () => {
+    it.skip('should be able to log in a user', () => {
         const action = {
-            type: 'TOGGLE_LOGIN',
-            id: 1
+            type: 'LOGOUT',
+            loggedIn: false
         }
-        const state = [
-            { name: 'Eric', email: 'eric@gmail.com', password: 'password', id: 5, loggedIn: false },
-            { name: 'Jessie', email: 'jessie@yahoo.com', password: 'password', id: 1, loggedIn: true },
-            { name: 'Victor', email: 'victor@hotmail.com', password: 'password', id: 6, loggedIn: false }
-        ]
-        const newState = [
-            { name: 'Eric', email: 'eric@gmail.com', password: 'password', id: 5, loggedIn: false },
-            { name: 'Jessie', email: 'jessie@yahoo.com', password: 'password', id: 1, loggedIn: false },
-            { name: 'Victor', email: 'victor@hotmail.com', password: 'password', id: 6, loggedIn: false }
-        ]
+        const state = 
+            { name: 'Eric', email: 'eric@gmail.com' , password:'password', id: 5 , loggedIn: true }
+        
+        const newState = 
+            { name: 'Eric', email: 'eric@gmail.com', password: 'password', id: 5, loggedIn: false }
+        
 
-        const result = user(state, action);
+        const result = isLoggedIn(state, action);
 
         expect(result).toEqual(newState);
-    })
+    });
 })
