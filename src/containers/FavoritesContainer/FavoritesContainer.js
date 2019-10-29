b709827f99566c20764912c87e054f6e5f2d5981
VPAbraham <vpa456@gmail.com>
2019-10-29 13:18:12 -0600

    Change favorites page styling and drill toggleFavorites down into favorits container

--- src/containers/FavoritesContainer/FavoritesContainer.js
+++ src/containers/FavoritesContainer/FavoritesContainer.js
@@ -6,8 +6,7 @@ import './FavoritesContainer.scss';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-export const FavoritesContainer = ({ favorites }) => {
-  console.log(favorites)
+export const FavoritesContainer = ({ favorites, toggleFavorites }) => {
   const favoritesCards = favorites.map(favorite => {
     return (
       <Card
@@ -15,16 +14,20 @@ export const FavoritesContainer = ({ favorites }) => {
         poster={`https://image.tmdb.org/t/p/w342${favorite.poster_path}`}
         title={favorite.title}
         favorited="true"
+        toggleFavorites={toggleFavorites}
       />
     )
   })
 
   return (
-    <div className="scroll-wrapper">
-      <div className="movies-container">
-        <img className="left-arrow arrow" src={images.leftArrow} alt="left arrow" />
-        {favoritesCards}
-        <img className="right-arrow arrow" src={images.rightArrow} alt="right arrow" />
+    <div className="favorites-container">
+      <h2 className="favorites-header">Favorited Films</h2>
+      <div className="scroll-wrapper">
+        <div className="movies-container">
+          <img className="left-arrow arrow" src={images.leftArrow} alt="left arrow" />
+          {favoritesCards}
+          <img className="right-arrow arrow" src={images.rightArrow} alt="right arrow" />
+        </div>
       </div>
     </div>
   )
