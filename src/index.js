1dc79616293a9dde6f281ce83476015058f2b603
VPAbraham <vpa456@gmail.com>
2019-10-28 20:40:55 -0600

    Remove some unnecessary files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter as Router} from 'react-router-dom'
 import App from './containers/App/App';
-import * as serviceWorker from './serviceWorker';
 import { rootReducer } from './reducers';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -20,8 +19,3 @@ const router = (
 )
 
 ReactDOM.render(router, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
